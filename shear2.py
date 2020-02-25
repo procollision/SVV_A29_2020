c5b62268bdf379e228af96ff8209c10f6ef66a2c
lasse <lasse@landergren.dk>
2020-02-25 16:09:48 +0100

    updated shear

---
+++ shear2.py
@@ -0,0 +1,85 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+import support_function as sup
+from sim_geom import *
+from CGFunction import *
+from aircraft_data import *
+
+#dt should be a multiple of 0.075
+tb = 0.01
+
+
+def get_q_shear(dt):
+    r = h_a/2
+    #compute stringer spacing over the skin
+    stringer_spacing = (r*np.pi+2*np.sqrt((C_a-r)*(C_a-r)+r*r))/N_st
+    print("stringer_spacing value %s" % stringer_spacing)
+    #stringer_spacing = r/2
+    #import coordiantes of stringer
+    stringer_cord_array,CG = centroid()
+    #stringer_cord_array = [[r/2,0],[0,0],[-r/2,0]]
+    #compute stringer area
+    stringer_area = (w_st+h_st)*w_st
+    
+
+    final_qlist = [[[[],[],[]],[[],[],[],[]]],[[[],[],[]],[[],[],[],[]]]]
+    #import geometry
+    geometry = get_geom(r,C_a,t_sk,t_sp,dt,stringer_spacing)
+    #geometry = get_geom(h,lw,tb,dt)
+    #di = dimension index
+    for di in range(np.shape(geometry)[0]):
+        #ci = cell index
+        for ci in range(np.shape(geometry)[1]):
+            intermidate_qlist = np.array([])
+            #for carrying over number of stringers over elements
+            B_final = np.array([])
+            #element index
+            for ei in range(np.shape(geometry[di][ci][0])[0]):
+                #obtain geometry information
+                func = geometry[di][ci][0][ei]
+                bounds = geometry[di][ci][1][ei]
+                stringer = geometry[di][ci][2][ei]
+                t = geometry[di][ci][3][ei]
+                ds = geometry[di][ci][4][ei]
+                offset = geometry[di][ci][5][ei]
+                B_list = geometry[di][ci][6][ei]
+                
+                test = bounds[1]+ds
+                
+                s_list = np.arange(bounds[0],bounds[1]+ds,ds)
+                B_index = [[]]*len(s_list)
+        
+                
+                #evaluate integral int y ds
+                
+                if len(intermidate_qlist) > 0:
+                    q_list = sup.integrate(func,ds,bounds)*t + intermidate_qlist[-1] #for the correct shear flow 
+                else:
+                    q_list = sup.integrate(func,ds,bounds)*t
+                
+                #generate the number of B's included in the domain
+                for i in range(len(s_list)):
+                    if stringer == 1:
+                        print(int((s_list[i]+offset)/(stringer_spacing*ds/dt)))
+                        print(ds/dt)
+                        B_index[i] = np.append(B_final,B_list[:int((s_list[i]+offset)/(stringer_spacing*ds/dt))])
+                    else:
+                        B_index[i]= B_final
+            
+                #add the terms for the stringers
+                
+                for i in range(len(q_list)):
+                    tempsum = 0
+                    for k in B_index[i]:                     
+                        tempsum += stringer_cord_array[int(k)][di]
+                    q_list[i] += stringer_area*tempsum
+                
+                B_final = B_index[-1]
+                
+                if ci == 1:
+                    q_list += final_qlist[0][0][1][round(len(final_qlist[0][0][1])/2)] 
+
+                final_qlist[di][ci][ei]=q_list
+    return final_qlist
+
+final_qlist = get_q_shear(0.001)
--- sim_geom.py
+++ sim_geom.py
@@ -7,13 +7,15 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     y1_stringer = []
     y1_dt_list = []
     y1_offset = []
+    y1_stringer_index = []
 
     y1_bounds.append([0,np.pi/2])
     y1.append(lambda theta: r*r*np.sin(theta))
     y1_thickness.append(ts)
     y1_stringer.append(1)
     y1_dt_list.append(dt/r)
-    y1_offset.append(s_space)
+    y1_offset.append(s_space/r)
+    y1_stringer_index.append(range(0,3))
 
     y1_bounds.append([0,2*r])
     y1.append(lambda s: -r*s+r)
@@ -21,14 +23,15 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     y1_stringer.append(0)
     y1_dt_list.append(dt)
     y1_offset.append(0)
+    y1_stringer_index.append([0])
 
-    y1_bounds.append([-np.pi/2,0])
-    y1.append(lambda theta: r*r*np.sin(theta))
+    y1_bounds.append([0.0,np.pi/2])
+    y1.append(lambda theta: r*r*np.sin(theta-r))
     y1_thickness.append(ts)
     y1_stringer.append(1)
     y1_dt_list.append(dt/r)
-    y1_offset.append((0.5*np.pi*r)%s_space)
-
+    y1_offset.append((0.5*np.pi*r)%s_space/r)
+    y1_stringer_index.append(range(15,17))
 
 
     y2 = []
@@ -37,6 +40,7 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     y2_stringer = []
     y2_dt_list = []
     y2_offset = []
+    y2_stringer_index = []
 
     y2_bounds.append([0,r])
     y2.append(lambda s: s)
@@ -44,20 +48,32 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     y2_stringer.append(0)
     y2_dt_list.append(dt)
     y2_offset.append(0)
+    y2_stringer_index.append([0])
 
-    y2_bounds.append([0,2*np.sqrt(r*r+(cl-r)*(cl-r))])
+    y2_bounds.append([0,np.sqrt(r*r+(cl-r)*(cl-r))])
     y2.append(lambda s: -r/(np.sqrt(r*r+(cl-r)*(cl-r)))*s+r)
     y2_thickness.append(ts)
     y2_stringer.append(1)
     y2_dt_list.append(dt)
     y2_offset.append(s_space-(0.5*np.pi*r)%s_space)
+    y2_stringer_index.append(range(3,9))
+
+    y2_bounds.append([0,np.sqrt(r*r+(cl-r)*(cl-r))])
+    y2.append(lambda s: -r/(np.sqrt(r*r+(cl-r)*(cl-r)))*s)
+    y2_thickness.append(ts)
+    y2_stringer.append(1)
+    y2_dt_list.append(dt)
+    y2_offset.append(s_space-(0.5*np.pi*r)%s_space)
+    y2_stringer_index.append(range(9,15))
+
 
     y2_bounds.append([0,r])
     y2.append(lambda s: s-r)
     y2_thickness.append(tb)
     y2_stringer.append(0)
     y2_dt_list.append(dt)
-    y1_offset.append(0)
+    y2_offset.append(0)
+    y2_stringer_index.append([0])
 
 
 
@@ -67,13 +83,15 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     z1_stringer = []
     z1_dt_list = []
     z1_offset = []
+    z1_stringer_index = []
 
     z1_bounds.append([0,np.pi/2])
     z1.append(lambda theta: r*r*np.cos(theta))
     z1_thickness.append(ts)
     z1_stringer.append(1)
     z1_dt_list.append(dt/r)
-    z1_offset.append(1)
+    z1_offset.append(s_space/r)
+    z1_stringer_index.append(range(0,3))
 
     z1_bounds.append([0,2*r])
     z1.append(lambda s: 0)
@@ -81,13 +99,15 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     z1_stringer.append(0)
     z1_dt_list.append(dt)
     z1_offset.append(0)
+    z1_stringer_index.append([0])
 
-    z1_bounds.append([-np.pi/2,0])
-    z1.append(lambda theta: r*r*np.cos(theta))
+    z1_bounds.append([0,np.pi/2])
+    z1.append(lambda theta: r*r*np.cos(theta)-r)
     z1_thickness.append(ts)
     z1_stringer.append(1)
     z1_dt_list.append(dt/r)
-    z1_offset.append((0.5*np.pi*r)%s_space)
+    z1_offset.append((0.5*np.pi*r)%s_space/r)
+    z1_stringer_index.append(range(15,17))
 
 
 
@@ -97,6 +117,7 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     z2_stringer = []
     z2_dt_list = []
     z2_offset = []
+    z2_stringer_index = []
 
     z2_bounds.append([0,r])
     z2.append(lambda s: 0)
@@ -104,6 +125,7 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     z2_stringer.append(0)
     z2_dt_list.append(dt)
     z2_offset.append(0)
+    z2_stringer_index.append([0])
 
     z2_bounds.append([0,np.sqrt(r*r+(cl-r)*(cl-r))])
     z2.append(lambda s: -s*np.sqrt((cl/r-1)*(cl/r-1)+1))
@@ -111,6 +133,7 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     z2_stringer.append(1)
     z2_dt_list.append(dt)
     z2_offset.append(s_space-(0.5*np.pi*r)%s_space)
+    z2_stringer_index.append(range(3,9))
 
     z2_bounds.append([0,np.sqrt(r*r+(cl-r)*(cl-r))])
     z2.append(lambda s: s*np.sqrt((cl/r-1)*(cl/r-1)+1)-(cl-r))
@@ -118,6 +141,7 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     z2_stringer.append(1)
     z2_dt_list.append(dt)
     z2_offset.append(0.5*s_space)
+    z2_stringer_index.append(range(9,15))
 
     z2_bounds.append([0,r])
     z2.append(lambda s: 0)
@@ -125,9 +149,8 @@ def get_geom(r,cl,ts,tb,dt,s_space):
     z2_stringer.append(0)
     z2_dt_list.append(dt)
     z2_offset.append(0)
+    z2_stringer_index.append([0])
 
-    y_s = [[y1,y1_bounds,y1_stringer,y1_thickness,y1_dt_list,y1_offset],[y2,y2_bounds,y2_stringer,y2_thickness,y2_dt_list,y2_offset]]
-    z_s = [[z1,z1_bounds,z1_stringer,z1_thickness,z1_dt_list,z1_offset],[z2,z2_bounds,z2_stringer,z2_thickness,z2_dt_list,z2_offset]]
+    y_s = [[y1,y1_bounds,y1_stringer,y1_thickness,y1_dt_list,y1_offset,y1_stringer_index],[y2,y2_bounds,y2_stringer,y2_thickness,y2_dt_list,y2_offset,y2_stringer_index]]
+    z_s = [[z1,z1_bounds,z1_stringer,z1_thickness,z1_dt_list,z1_offset,z1_stringer_index],[z2,z2_bounds,z2_stringer,z2_thickness,z2_dt_list,z2_offset,z2_stringer_index]]
     return[y_s,z_s]
-
-
