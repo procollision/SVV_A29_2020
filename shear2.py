6b0c5ab3d65aa6e7c40301361f1961ae160e4230
lasse <lasse@landergren.dk>
2020-02-26 09:11:26 +0100

    changes to shear fixing cg location

--- shear2.py
+++ shear2.py
@@ -13,7 +13,7 @@ def get_q_shear(dt):
     r = h_a/2
     #compute stringer spacing over the skin
     stringer_spacing = (r*np.pi+2*np.sqrt((C_a-r)*(C_a-r)+r*r))/N_st
-    print("stringer_spacing value %s" % stringer_spacing)
+    #print("stringer_spacing value %s" % stringer_spacing)
     #stringer_spacing = r/2
     #import coordiantes of stringer
     stringer_cord_array,CG = centroid()
@@ -24,13 +24,12 @@ def get_q_shear(dt):
 
     final_qlist = [[[[],[],[]],[[],[],[],[]]],[[[],[],[]],[[],[],[],[]]]]
     #import geometry
-    geometry = get_geom(r,C_a,t_sk,t_sp,dt,stringer_spacing)
+    geometry = get_geom(r,C_a,t_sk,t_sp,dt,stringer_spacing,CG[1])
     #geometry = get_geom(h,lw,tb,dt)
     #di = dimension index
     for di in range(np.shape(geometry)[0]):
         #ci = cell index
         for ci in range(np.shape(geometry)[1]):
-            intermidate_qlist = np.array([])
             #for carrying over number of stringers over elements
             B_final = np.array([])
             #element index
@@ -44,7 +43,6 @@ def get_q_shear(dt):
                 offset = geometry[di][ci][5][ei]
                 B_list = geometry[di][ci][6][ei]
                 
-                test = bounds[1]+ds
                 
                 s_list = np.arange(bounds[0],bounds[1]+ds,ds)
                 B_index = [[]]*len(s_list)
@@ -52,16 +50,17 @@ def get_q_shear(dt):
                 
                 #evaluate integral int y ds
                 
-                if len(intermidate_qlist) > 0:
-                    q_list = sup.integrate(func,ds,bounds)*t + intermidate_qlist[-1] #for the correct shear flow 
+                if ei!= 0:
+                    q_list = sup.integrate(func,ds,bounds)*t + final_qlist[di][ci][ei-1][-1] #for the correct shear flow 
+                    #print("final_qlist %s" % final_qlist[di][ci][ei-1][-1])
                 else:
                     q_list = sup.integrate(func,ds,bounds)*t
                 
                 #generate the number of B's included in the domain
                 for i in range(len(s_list)):
                     if stringer == 1:
-                        print(int((s_list[i]+offset)/(stringer_spacing*ds/dt)))
-                        print(ds/dt)
+                        #print(int((s_list[i]+offset)/(stringer_spacing*ds/dt)))
+                        #print(ds/dt)
                         B_index[i] = np.append(B_final,B_list[:int((s_list[i]+offset)/(stringer_spacing*ds/dt))])
                     else:
                         B_index[i]= B_final
@@ -70,8 +69,9 @@ def get_q_shear(dt):
                 
                 for i in range(len(q_list)):
                     tempsum = 0
-                    for k in B_index[i]:                     
-                        tempsum += stringer_cord_array[int(k)][di]
+                    for k in B_index[i]:
+                        #print(CG)                     
+                        tempsum += stringer_cord_array[int(k)][di]#+CG[di]
                     q_list[i] += stringer_area*tempsum
                 
                 B_final = B_index[-1]
@@ -82,4 +82,4 @@ def get_q_shear(dt):
                 final_qlist[di][ci][ei]=q_list
     return final_qlist
 
-final_qlist = get_q_shear(0.001)
+#final_qlist = get_q_shear(0.001)
