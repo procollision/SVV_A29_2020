7e9f45d9aef4295b9ca43f4ba44ab3ed3ba88332
TimoVMaarschalk <61225734+TimoVMaarschalk@users.noreply.github.com>
2020-02-26 13:30:16 +0100

    Add files via upload

---
+++ arm2.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+def get_arm(r,cl,ts,tb,dt,s_space):
+    y1 = []
+    y1_bounds = []
+    y1_dt_list = []
+    #yi.append are the arms of the shearflow
+
+    y1_bounds.append([0,np.pi/2])
+    y1.append(lambda theta: r*r)
+    y1_dt_list.append(dt/r)
+
+    y1_bounds.append([0,2*r])
+    y1.append(lambda s: 0)
+    y1_dt_list.append(dt)
+
+
+    y1_bounds.append([-np.pi/2,0])
+    y1.append(lambda theta: -r*r)
+    y1_dt_list.append(dt/r)
+
+    y2 = []
+    y2_bounds = []
+    y2_dt_list = []
+
+    y2_bounds.append([0,r])
+    y2.append(lambda s: 0)
+    y2_dt_list.append(dt)
+
+    y2_bounds.append([0,np.sqrt(r*r+(cl-r)*(cl-r))])
+    y2.append(lambda s: (((np.sqrt(r*r+(cl-r)*(cl-r))-s)*r+s*(cl-r))/np.sqrt(r*r+(cl-r)*(cl-r))))
+    y2_dt_list.append(dt)
+
+    y2_bounds.append([0,np.sqrt(r*r+(cl-r)*(cl-r))])
+    y2.append(lambda s: (((np.sqrt(r*r+(cl-r)*(cl-r))-s)*(cl-r)+s*r)/np.sqrt(r*r+(cl-r)*(cl-r))))
+    y2_dt_list.append(dt)
+
+    y2_bounds.append([0,r])
+    y2.append(lambda s: 0)
+    y2_dt_list.append(dt)
+
+    y_a = [[y1,y1_bounds,y1_dt_list],[y2,y2_bounds,y2_dt_list]]
+    return [y_a]
